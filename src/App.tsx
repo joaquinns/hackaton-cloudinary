c8c3b34ca88069254ee1284cb4a5fd73cf55f0ee
joaquinns <joaquindevv96@gmail.com>
2023-03-04 18:21:28 -0400

    refactor components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,52 @@
-import { useDropzone } from 'react-dropzone'
+import { Backgroundsvg } from './components/Backgroundsvg'
 import { EditBoard } from './components/EditBoard'
 import { Layout } from './components/Layout'
 import { UploadIcon } from './components/UploadIcon'
 import { useEditBoard } from './hooks/useEditBoard'
 
 function App() {
-  const { newImage, onDrop } = useEditBoard()
-  const { getInputProps, getRootProps } = useDropzone({
-    onDrop,
-    maxFiles: 1
-  })
+  const { newImage, getInputProps, getRootProps, style, iconColor } =
+    useEditBoard()
 
   return (
     <Layout>
-      <div className='container mx-auto px-4 lg:px-0 min-h-screen'>
-        <h2 className='text-6xl text-center text-slate-900'>compa</h2>
-
+      <div className='container mx-auto px-4 py-8 lg:px-0 min-h-screen z-50'>
         <div className='flex justify-center items-center min-h-screen'>
-          <div className='flex flex-1 min-h-screen justify-center'>
+          <div className='flex flex-1 min-h-screen justify-center items-center'>
             {newImage ? (
               <>
                 <EditBoard image={newImage} />
               </>
             ) : (
-              <div
-                className='flex items-center justify-center w-full lg:w-1/2'
-                {...getRootProps()}
-              >
+              <div className='flex flex-col lg:flex-row gap-4 items-center justify-center w-full h-full lg:w-1/2'>
+                <div className='flex flex-col justify-center items-center w-full'>
+                  <h2 className='text-5xl text-center text-gray-700 font-semibold my-8 p-2 bg-[#f5deb3] rounded'>
+                    Edit your images!
+                  </h2>
+                  <img
+                    src='https://res.cloudinary.com/compape/image/upload/e_blur/e_brightness:0/e_fill_light:0/e_saturation:0/c_fill,h_250,w_250/qsnek4fuddgebtenhqk2?_a=ATCqVAA0'
+                    className='w-[200px] h-[200px]'
+                    alt='image'
+                  />
+                </div>
+                <Backgroundsvg />
                 <label
+                  {...getRootProps({
+                    style
+                  })}
                   htmlFor='dropzone-file'
-                  className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600'
+                  className='flex flex-col items-center justify-center w-full h-64 p-4 rounded-lg cursor-pointer border border-1 border-gray-300 700 hover:bg-gray-200 shadow-md'
                 >
                   <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                    <UploadIcon />
+                    <UploadIcon classColor={iconColor()} />
                     <p className='mb-2 text-sm text-gray-500 dark:text-gray-400'>
-                      <span className='font-semibold'>Click to upload</span> or
-                      drag and drop
+                      <span className='font-bold'>
+                        Click to upload an image
+                      </span>{' '}
+                      or drag and drop
                     </p>
                     <p className='text-xs text-gray-500 dark:text-gray-400'>
-                      SVG, PNG, JPG or GIF (MAX. 800x400px)
+                      PDF to SVG, PNG, JPG or GIF.
                     </p>
                   </div>
                   <input
